306f89de4d8f5d83281d41080c0adabcc7c060f1
Kush Parsai <kushparsai1@gmail.com>
2026-01-07 21:21:44 +0530

    j

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
 import AuthProvider from "./context/AuthProvider";
+import { CartProvider } from "./context/CartContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <CartProvider>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </CartProvider>
     </AuthProvider>
   </React.StrictMode>
 );

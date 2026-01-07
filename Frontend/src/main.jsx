48833018ce99920dfb8ebfacd7f1bb7a923f8201
Kush Parsai <kushparsai1@gmail.com>
2026-01-07 21:03:50 +0530

    n

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
-import AuthProvider from "./context/AuthProvider";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </AuthProvider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );

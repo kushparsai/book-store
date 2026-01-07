4116a2f3602cc0c253b5be61c9bd323c05155f33
Kush Parsai <kushparsai1@gmail.com>
2026-01-07 20:53:33 +0530

    new

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,9 +1,7 @@
-export default {
-  darkMode: "class", // ✅ MUST
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  base: "/book-store/",
-};
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/book-store/",   // ✅ YAHI HONA CHAHIYE
+});
